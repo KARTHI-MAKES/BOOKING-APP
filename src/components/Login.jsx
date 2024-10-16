d9fd11b16a66984c16523202ded569ca667c2576
KARTHI-MAKES <karthiyamunarani5@gmail.com>
2024-10-16 17:24:08 +0530

    Add files via upload

---
+++ src/components/Login.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import {useNavigate} from 'react-router-dom'
+
+const Login = () => {
+  const navigate=useNavigate()
+  const { login, error, user } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+    if(user)
+    navigate("events")
+    
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Enter email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="Enter password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type="submit">Login</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </form>
+  );
+};
+
+export default Login;
